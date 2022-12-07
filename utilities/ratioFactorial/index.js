9d346b1f6b2c7550f8a862e957987d8385aaca70
shiva <shivarama99666@gmail.com>
2022-12-07 13:48:10 +0000

    calculte ratio and fraction

---
+++ utilities/ratioFactorial/index.js
@@ -0,0 +1,11 @@
+let factorialOfNumber = require("../factorial/index.js");
+
+let ratioOfTwoNumbers = require("../ratio/index.js");
+
+const ratioAndFactorial = (num1, num2, num3) => {
+  const ratio = ratioOfTwoNumbers(num1, num2);
+  const factorial = factorialOfNumber(num3);
+
+  return { ratio, factorial };
+};
+module.exports = ratioAndFactorial;
